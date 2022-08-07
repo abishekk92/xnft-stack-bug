ae4df8fd78da7250e3ced598ddaf410773e0b940
abishekk92 <bhat.abishek@gmail.com>
2022-08-07 18:08:18 -0400

    minimal app that doesn't work

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import ReactXnft, { Text, View } from "react-xnft";
+import ReactXnft, { Stack, Text, View } from "react-xnft";
 
 //
 // On connection to the host environment, warm the cache.
@@ -10,7 +10,54 @@ ReactXnft.events.on("connect", () => {
 export function App() {
   return (
     <View>
-      <Text>Hello, World!</Text>
+      <StackTest />
+    </View>
+  );
+}
+
+function StackTest() {
+  return (
+    <Stack.Navigator
+      initialRoute={{ name: "foo" }}
+      options={({ route }) => {
+        switch (route.name) {
+          case "foo":
+            return {
+              title: "Stack 1",
+            };
+          case "bar":
+            return {
+              title: "Stack 2",
+            };
+          default:
+            throw new Error("unknown route");
+        }
+      }}
+      style={{}}
+    >
+      <Stack.Screen
+        name={"foo"}
+        component={(props: any) => <FooComponent {...props} />}
+      />
+      <Stack.Screen
+        name={"bar"}
+        component={(props: any) => <FooComponent {...props} />}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function FooComponent(props: {}) {
+  return (
+    <View>
+      <Text
+        style={{
+          height: "100%",
+          color: "#FFFFFF",
+        }}
+      >
+        This is a text
+      </Text>
     </View>
   );
 }
